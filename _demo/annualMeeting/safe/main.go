8756798f80fb6358ea7ecf26848fcd468263e786
jiangbolun <mengdingbuhui@163.com>
2025-03-07 15:09:00 +0800

    add annual meeting safe

---
+++ _demo/annualMeeting/safe/main.go
@@ -0,0 +1,89 @@
+/**
+ * 年会抽奖程序
+ * 不是线程安全
+ * 基础功能：
+ * 1 /import 导入参与名单作为抽奖的用户
+ * 2 /lucky 从名单中随机抽取用户
+ * 测试方法：
+ * curl http://localhost:8080/
+ * curl --data "users=yifan,yifan2" http://localhost:8080/import
+ * curl http://localhost:8080/lucky
+ */
+
+package main
+
+import (
+	"fmt"
+	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/mvc"
+	"math/rand"
+	"strings"
+	"sync"
+	"time"
+)
+
+var userList []string // var userList → 相当于Spring中的单例Bean（但Go的全局变量需要特别注意并发安全）
+var mu sync.Mutex
+
+type lotteryController struct { // 抽奖的控制器（类似@RestController）
+	Ctx iris.Context // 依赖注入请求上下文（类似Spring的@Autowired HttpServletRequest）
+}
+
+func (c *lotteryController) Get() string { // GET http://localhost:8080/ （等效@GetMapping）
+	count := len(userList)
+	return fmt.Sprintf("当前总共参与抽奖的用户数: %d\n", count)
+}
+
+// POST http://localhost:8080/import
+func (c *lotteryController) PostImport() string {
+	strUsers := c.Ctx.FormValue("users")
+	users := strings.Split(strUsers, ",")
+	mu.Lock()
+	defer mu.Unlock()
+	count1 := len(userList)
+	for _, u := range users {
+		u = strings.TrimSpace(u)
+		if len(u) > 0 {
+			// 导入用户
+			userList = append(userList, u)
+		}
+	}
+	count2 := len(userList)
+	return fmt.Sprintf("当前总共参与抽奖的用户数: %d，成功导入用户数: %d\n", count2, (count2 - count1))
+}
+
+// GET http://localhost:8080/lucky
+func (c *lotteryController) GetLucky() string {
+	mu.Lock()
+	defer mu.Unlock()
+	count := len(userList)
+	if count > 1 {
+		seed := time.Now().UnixNano()                                // rand内部运算的随机数
+		index := rand.New(rand.NewSource(seed)).Int31n(int32(count)) // rand计算得到的随机数
+		user := userList[index]                                      // 抽取到一个用户
+		userList = append(userList[0:index], userList[index+1:]...)  // 移除这个用户
+		return fmt.Sprintf("当前中奖用户: %s, 剩余用户数: %d\n", user, count-1)
+	} else if count == 1 {
+		user := userList[0]
+		userList = userList[0:0]
+		return fmt.Sprintf("当前中奖用户: %s, 剩余用户数: %d\n", user, count-1)
+	} else {
+		return fmt.Sprintf("已经没有参与用户，请先通过 /import 导入用户 \n")
+	}
+
+}
+
+func newApp() *iris.Application { // newApp() → 类似@SpringBootApplication配置类
+	app := iris.New()
+	mvc.New(app.Party("/")).Handle(&lotteryController{}) // mvc.New() → 类似Spring MVC的@Controller自动扫描
+	return app
+}
+
+func main() {
+	app := newApp()
+
+	userList = make([]string, 0)
+	mu = sync.Mutex{}
+
+	app.Run(iris.Addr(":8080"))
+}
